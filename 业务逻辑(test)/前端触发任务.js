e2ad15097558cafefc5110772d3ca4f4822a7696
zhang513 <zhq00000@gmail.com>
2024-08-07 17:42:57 +0800

    更新

--- 业务逻辑(test)/前端触发任务.js
+++ 业务逻辑(test)/前端触发任务.js
@@ -1,3 +1,12 @@
+// 可能传递参数如下：
+// 任务ID
+// 初始化任务链接
+// 子节点列表
+// 部署类型 # TODO 默认Scrapy / 直接部署项目
+// 数据落库 # TODO 数据类型、数据库类型
+// 安装爬虫环境 # TODO
+
+
 fetch('http://main-node/api/start-task', {
     method: 'POST',
     headers: {
